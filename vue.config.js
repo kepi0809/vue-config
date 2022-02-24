5abeff21ef9af502ce9c998f3068f5391d282065
Sándor Képiró <kepi0809@gmail.com>
2022-02-24 13:38:32 +0100

    latest vue config

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,4 @@
-const glob = require('glob');
-const path = require('path');
 const fs = require('fs');
-
-const RUNNING_ON_DEV = process.env.VUE_APP_HM_APPLICATION_ENV === 'development';
 
 const httpsOption = () => {
     try {
@@ -12,7 +8,7 @@ const httpsOption = () => {
         };
     } catch {
         // Could not find SSL certificates for the webpack-server. Self-signed certificate will be used.
-        return true;
+        return {};
     }
 };
 
@@ -31,31 +27,15 @@ module.exports = {
     },
     productionSourceMap: false,
     configureWebpack: {
-        devtool: RUNNING_ON_DEV ? 'cheap-module-eval-source-map' : 'source-map',
+        devtool: RUNNING_ON_DEV ? 'eval-cheap-module-source-map' : 'source-map',
     },
     chainWebpack: (config) => {
-        const mockSchemas = [];
-
-        if (RUNNING_ON_DEV) {
-            // Find all mock schemas.
-            const schemas = glob.sync('*.schema.gql', {
-                cwd: 'src/graphql/mocking/schemas/',
-                ignore: ['FullMock.schema.gql'],
-                absolute: true,
-            });
-
-            // Use mock schema only in development build.
-            mockSchemas.push(...schemas);
-        }
-
         config.module
             .rule('graphql-introspection')
             .test(/\.igql$/)
             .use('introspection-loader')
             .loader('./.introspection-loader.js')
-            .options({
-                schemas: ['./schema.graphql', ...mockSchemas],
-            })
+            .options({ schemas: ['./schema.graphql'] })
             .end();
 
         // Enable loading of .gql files containing graphql queries
@@ -75,21 +55,15 @@ module.exports = {
             analyzerMode: 'disabled',
         },
     },
-    transpileDependencies: ['@rd-internal/rd-vue-gtm'],
     lintOnSave: false,
     devServer: {
         proxy: `https://${process.env.HM_SANDBOX_HOSTNAME}/`,
         host: '0.0.0.0',
-        publicPath: `https://${process.env.HM_SANDBOX_HOSTNAME}:8080/`,
-        disableHostCheck: true,
-        https: httpsOption(),
-        watchOptions: {
-            poll: true,
-            ignored: [
-                path.resolve('./node_modules'),
-                path.resolve('./tests'),
-                path.resolve('./docs'),
-            ],
+        devMiddleware: { publicPath: `https://${process.env.HM_SANDBOX_HOSTNAME}:8080/` },
+        allowedHosts: 'all',
+        server: {
+            type: 'https',
+            options: httpsOption(),
         },
     },
 };
